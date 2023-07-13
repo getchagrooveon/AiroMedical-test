70212afabf59243492adf2c04282a86bdf407fef
getchagrooveon <eugene.osipov.1989@gmail.com>
2023-07-13 23:58:51 +0300

    Update RecipeDetails.jsx

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,15 +1,15 @@
-import { useStore } from 'components/store';
+// import { useStore } from 'components/store';
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const RecipeDetails = () => {
-  const getRecipeById = useStore(store => store.getRecipeById);
+  //   const getRecipeById = useStore(store => store.getRecipeById);
   const info = useParams();
 
   console.log(info.recipeId);
 
   useEffect(() => {
-    getRecipeById();
+    // getRecipeById();
   }, []);
 
   return <div>RecipeDetails {info.recipeId}</div>;
